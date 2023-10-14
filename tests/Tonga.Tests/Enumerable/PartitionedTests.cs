443ef894f83a9d2a7f5463383fc3b0150676069c
meerow <meerow@mirmx.de>
2023-10-14 18:31:53 +0200

    refactor: rename lists

--- tests/Tonga.Tests/Enumerable/PartitionedTests.cs
+++ tests/Tonga.Tests/Enumerable/PartitionedTests.cs
@@ -13,8 +13,7 @@ namespace Tonga.Enumerable.Test
             Assert.Equal(
                 2,
                 new LengthOf(
-                    new Partitioned<string>(
-                        1,
+                    new Partitioned<string>(1,
                         new List<string>() { "hokus", "pokus" }
                     )
                 ).Value()

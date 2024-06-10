0cdce9ee992bc490c62ef7f70662bf3a350e0154
Meerow <meerow@mirmx.de>
2024-06-10 16:52:57 +0200

    feat: text to lines

---
+++ src/Tonga/Enumerable/Lines.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Tonga.Text;
+
+namespace Tonga.Enumerable
+{
+    /// <summary>
+    /// Lines in a given text.
+    /// </summary>
+    public sealed class Lines : IEnumerable<string>
+    {
+        private readonly IText source;
+        private readonly bool skipEmpty;
+
+        /// <summary>
+        /// Lines in a given text.
+        /// </summary>
+        public Lines(string source, bool skipEmpty = false) : this(AsText._(source), skipEmpty)
+        { }
+
+        /// <summary>
+        /// Lines in a given text.
+        /// </summary>
+        public Lines(IText source, bool skipEmpty = false)
+        {
+            this.source = source;
+            this.skipEmpty = skipEmpty;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            using (StringReader reader = new StringReader(this.source.AsString()))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if(!(skipEmpty && String.IsNullOrEmpty(line)))
+                        yield return line;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        /// <summary>
+        /// Lines in a given text.
+        /// </summary>
+        public static IEnumerable<string> _(IText source) =>
+            new Lines(source);
+    }
+}
---
+++ tests/Tonga.Tests/Enumerable/LinesTest.cs
@@ -0,0 +1,37 @@
+using System;
+using Tonga.Enumerable;
+using Xunit;
+
+namespace Tonga.Tests.Enumerable
+{
+    public sealed class LinesTest
+    {
+        [Fact]
+        public void SplitsLines()
+        {
+            Assert.Equal(
+                new string[] { "1", "2", "3" },
+                new Lines("1\r\n2\r\n3")
+            );
+        }
+
+        [Fact]
+        public void SkipsEmpty()
+        {
+            Assert.Equal(
+                new string[] { "1", "2", "4" },
+                new Lines("1\r\n2\r\n\r\n4\r\n", skipEmpty: true)
+            );
+        }
+
+        [Fact]
+        public void IncludesEmpty()
+        {
+            Assert.Equal(
+                new string[] { "1", "2", "", "4" },
+                new Lines("1\r\n2\r\n\r\n4\r\n", skipEmpty: false)
+            );
+        }
+    }
+}
+

35bdc4bf5b7631d7fbedecf34a1497b07ecfad25
Meerow <meerow@mirmx.de>
2025-05-18 16:29:00 +0200

    test: add missing test

--- tests/Tonga.Tests/Pipe/AsPipeTest.cs
+++ tests/Tonga.Tests/Pipe/AsPipeTest.cs
@@ -6,13 +6,24 @@ namespace Tonga.Tests.Pipe
     public sealed class AsPipeTest
     {
         [Fact]
-        public void Vields()
+        public void Yields()
         {
             Assert.Equal(
                 1,
                 AsPipe._<int, int>(
                     _ => 1
                 ).Yield(2)
+            );
+        }
+
+        [Fact]
+        public void YieldsFromInput()
+        {
+            Assert.Equal(
+                9,
+                AsPipe._<int, int>(
+                    ipt => ipt
+                ).Yield(9)
             );
         }
     }
